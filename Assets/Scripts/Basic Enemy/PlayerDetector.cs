51baf276f49e730ab4d7657b344fca1f1e054f51
Terrence Timmons <terrence.timmons@gmail.com>
2025-03-05 15:58:17 -0500

    Added Player Detector

--- Assets/Scripts/Basic Enemy/PlayerDetector.cs
+++ Assets/Scripts/Basic Enemy/PlayerDetector.cs
@@ -2,15 +2,71 @@ using UnityEngine;
 
 public class PlayerDetector : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Collider _sphereCollider;
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _forgetTime; // Used to determine when a target is forgotten
+    [SerializeField] private bool _playerDetected;
+    [SerializeField] private bool _inRange;
+
+    private float _currentForgetTime;
+
+    private void Awake()
     {
-        
+        _sphereCollider = GetComponent<SphereCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _playerDetected = false;
+        _inRange = false;
+        _currentForgetTime = 0;
+    }
+
+    private void Update()
+    {
+        if (_playerDetected && !_inRange)
+        {
+            _currentForgetTime += Time.deltaTime;
+            if(_currentForgetTime >= _forgetTime)
+            {
+                // Forget
+                _playerDetected = false;
+                _inRange = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!_playerDetected)
+        {
+            if(other.CompareTag("Player"))
+            {
+                _player = other.transform;
+                _sphereCollider.enabled = true;
+                _playerDetected = true;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(_playerDetected)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _currentForgetTime = 0;
+                _inRange = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(_inRange)
+        {
+            if (other.CompareTag("Player"))
+                _inRange = false;
+        }
     }
 }
