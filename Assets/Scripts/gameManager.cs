4bb90cfcd58141f80367afdfd101837eff5624d6
Terrence Timmons <terrence.timmons@gmail.com>
2025-03-27 04:35:26 -0400

    Narrowing down the issue
    Removed Enemy prefab from scene and Spawner that was added.
    Detatched Final Map in Final Scene from the prefab.

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -47,7 +47,6 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         if (instance == null) instance = this;
-        else Destroy(this);
     }
 
     private void Start()
